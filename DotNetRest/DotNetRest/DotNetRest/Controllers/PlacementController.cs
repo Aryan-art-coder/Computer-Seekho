996822185f808f363b56dd963d60ca5b8226adde
Aryan-art-coder <aryanbaluni574@gmail.com>
2025-09-04 15:58:25 +0530

    Delete DotNetRest/DotNetRest directory

--- DotNetRest/DotNetRest/DotNetRest/Controllers/PlacementController.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DotNetRest.Controllers
-{
-    public class PlacementController
-    {
-    }
-}
--- DotNetRest/DotNetRest/DotNetRest/Models/EnquiryClosurePatchDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace DotNetRest.Models
-{
-    public class EnquiryClosurePatchDTO
-    {
-        public int? ClosureReasonId { get; set; }
-        public string? ClosureReason { get; set; }
-    }
-}
